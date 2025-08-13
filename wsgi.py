36441dafdb962a2f89e7c49cedac909276c0db20
artbellson <69949962+artbellson@users.noreply.github.com>
2025-08-13 12:59:36 +0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -7,7 +7,8 @@ import os
 from app import create_app
 
 # Create application instance
-application = create_app(os.environ.get('FLASK_ENV') or 'production')
+config_name = os.environ.get('FLASK_ENV') or 'production'
+application = create_app(config_name)
 
 if __name__ == "__main__":
     application.run()
